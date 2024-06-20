9017265066684dabc8a86108c0102d9227ba55db
Abdi Naavi'al Umam Suleman <109885367+abdinaavial17@users.noreply.github.com>
2024-06-20 12:58:13 +0800

    Add files via upload

---
+++ views/user/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+    <title>sipti app</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Playball&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="../../src/fron/animate/animate.min.css" rel="stylesheet">
+    <link href="../../src/fron/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+    <link href="../../src/fron/lib/owlcarousel/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="../../src/fron/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="../../src/fron/css/style.css" rel="stylesheet">
+
+    <!-- Web App Manifest -->
+    <link rel="manifest" href="./manifest.json">
+</head>
+<body>
+    <div class="container-fluid nav-bar">
+        <div class="container">
+            <nav class="navbar navbar-light navbar-expand-lg py-4">
+                <a href="index.php" class="navbar-brand">
+                    <h1 class="text-primary fw-bold mb-0">Sipti<span class="text-dark">App</span> </h1>
+                </a>
+                <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="fa fa-bars text-primary"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav mx-auto">
+                        <a href="index.php" class="nav-item nav-link active">Home</a>
+                        <a href="tempat_wisata.php" class="nav-item nav-link">Destinasi liburan</a>
+                        <a href="about.php" class="nav-item nav-link">About</a>
+                    </div>
+                    <a href="../login.php" class="btn btn-primary py-2 px-4 d-none d-xl-inline-block rounded-pill">Login/register</a>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    <div class="container-fluid bg-light py-6 my-6 mt-0">
+        <div class="container">
+            <div class="row g-5 align-items-center">
+                <div class="col-lg-7 col-md-12">
+                    <small class="d-inline-block fw-bold text-dark text-uppercase bg-light border border-primary rounded-pill px-4 py-1 mb-4 animated bounceInDown">Welcome to Sipti App</small>
+                    <h1 class="display-1 mb-4 animated bounceInDown">Web  <span class="text-primary">Distinasi</span>Untuk Anda Yang Sedang Mencari Liburan</h1>
+                    <a href="" class="btn btn-primary border-0 rounded-pill py-3 px-4 px-md-5 me-4 animated bounceInLeft">Cari Distinasi</a>
+                    <a href="" class="btn btn-primary border-0 rounded-pill py-3 px-4 px-md-5 animated bounceInLeft">Informasi Liburan</a>
+                </div>
+                <div class="col-lg-5 col-md-12">
+                    <img src="../../src/fron/img/animasi1.jpg" class="img-fluid rounded animated zoomIn" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="footer-logo" style="padding: 10px;"><h1 align="center">SIPTI APP</h1></div>
+        <div class="footer-copyright"style="padding: 10px;"><p align="center">Copyright 2024</p></div>
+    </div>
+</body>
+</html>
---
+++ views/user/tempat_wisata.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+    <title>sipti app</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Playball&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="../../src/fron/animate/animate.min.css" rel="stylesheet">
+    <link href="../../src/fron/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+    <link href="../../src/fron/lib/owlcarousel/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="../../src/fron/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="../../src/fron/css/style.css" rel="stylesheet">
+
+    <!-- Web App Manifest -->
+    <link rel="manifest" href="./manifest.json">
+</head>
+<body>
+    <div class="container-fluid nav-bar">
+        <div class="container">
+            <nav class="navbar navbar-light navbar-expand-lg py-4">
+                <a href="index.php" class="navbar-brand">
+                    <h1 class="text-primary fw-bold mb-0">Sipti<span class="text-dark">App</span> </h1>
+                </a>
+                <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="fa fa-bars text-primary"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav mx-auto">
+                        <a href="index.php" class="nav-item nav-link">Home</a>
+                        <a href="tempat_wisata.php" class="nav-item nav-link active">Destinasi liburan</a>
+                        <a href="about.php" class="nav-item nav-link">About</a>
+                    </div>
+                    <a href="../login.php" class="btn btn-primary py-2 px-4 d-none d-xl-inline-block rounded-pill">Login/register</a>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    <div class="container-fluid bg-light py-6 my-6 mt-0">
+        <div class="container">
+            <h1>Destinasi liburan</h1>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="footer-logo" style="padding: 10px;"><h1 align="center">SIPTI APP</h1></div>
+        <div class="footer-copyright"style="padding: 10px;"><p align="center">Copyright 2024</p></div>
+    </div>
+</body>
+</html>
